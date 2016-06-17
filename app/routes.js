7969b51cb30926fdbe9a5becc77a1a6a0e3f1b56
MartinSeibert <howtoemailmartin@gmail.com>
2016-06-16 20:18:47 -0400

    added routes and a simple index.html

---
+++ app/routes.js
@@ -0,0 +1,36 @@
+// grab the show model
+var Show = require('./models/show');
+
+module.exports = function(app){
+
+	// server routes ================================================
+	// handle things like api calls
+	// authentication routes
+
+	// sample api route
+	app.get('/api/shows', function(req, res){
+
+		//use mongoose to get all shows in the database
+		Show.find(function(err, shows){
+			// if there is an error retrieving, send the error
+			// nothing after res.send(err) will execute
+			if(err){
+				res.send(err);
+			}
+
+			res.json(shows); // return all shows in json format
+
+
+		});
+	});
+
+	// route to handle creating shows goes here (app.post)
+	// route to handle delete goes here(app.delete)
+
+	// frontend routes =========================================
+	// route to handle all angular requests
+	app.get('*', function(req, res){
+		res.sendfile('./public/views/index.html'); // load our public/index.html file
+	});
+
+};
